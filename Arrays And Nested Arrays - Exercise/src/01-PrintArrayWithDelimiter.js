137b6bf8f76270c7fe2e7aa3a58427b1530461de
Bojidar Kaloyanov <155442466+Kaloyanov5@users.noreply.github.com>
2024-03-16 13:52:31 +0200

    Added 01-PrintArrayWithDelimiter

---
+++ Arrays And Nested Arrays - Exercise/src/01-PrintArrayWithDelimiter.js
@@ -0,0 +1,11 @@
+function printArrayDelimiter(array, delimiter){
+    console.log(array.join(delimiter));
+}
+
+printArrayDelimiter(['How about no?', 
+'I',
+'will', 
+'not', 
+'do', 
+'it!'], 
+'_');
