8cf548828013aa175223d8e2408c34333f7d6b69
Kaloyanov5 <kaloyanovbojidar@gmail.com>
2024-04-14 18:23:50 +0300

    Added 04-ShowMore

---
+++ DOM Introduction - Lab/src/04-ShowMore/show-more.js
@@ -0,0 +1,6 @@
+function showText(){
+    const link = document.getElementById("more");
+    const hiddenText = document.getElementById("text");
+    hiddenText.style.display = "inline";
+    link.style.display = "none";
+}
